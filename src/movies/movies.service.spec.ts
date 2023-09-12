1a22de436afef1671b327e102b498b14ff3169f9
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-09-12 17:14:00 +0900

    유닛 테스트 연습

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -16,7 +17,33 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should be 4', () => {
-    expect(2 + 3).toEqual(5);
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array); // getAll 함수의 결과는 배열이 되어야 한다.
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        // 테스트 용 영화를 생성하고
+        title: 'test movie',
+        genres: ['test'],
+        year: 2023,
+      });
+      const movie = service.getOne(1); // 영화의 ID가 1인 영화를 변수 movie에 할당
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999); // 존재하지 않는 영화의 ID 999을 호출하여 에러를 발생시킨다.
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
   });
 });
