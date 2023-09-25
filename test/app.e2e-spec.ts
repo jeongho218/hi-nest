c70afced265aa5f7bebc7def35248eb813e5a5bb
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-09-25 23:54:05 +0900

    e2e 테스트 localhost/movies 엔드 포인트에 대해서 GET, POST, DELETE에 대한 테스트를  app.e2e-spec.ts 파일 내에서 모두 처리할 수 있게됨

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import * as request from 'supertest'; // e2e 테스트
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -17,8 +17,26 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/') // URL '/'에 대한 request(controller, service, pipe)를 모두 테스트한다.
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()) // http://localhost:3000/과 동일하다
+        .get('/movies')
+        .expect(200)
+        .expect([]);
+    });
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ title: 'Test', year: 2023, genres: ['test'] })
+        .expect(201);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
 });
