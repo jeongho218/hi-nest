97934e93c2a0c7e6f9cb01826f15ca7c545e640c
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-09-26 00:44:27 +0900

    1. localhost/movies/:id 엔드 포인트에 대한 테스트 코드 작성 2. getOne, update 테스트를 위해 string이던 파라미터(id)를 number로 자동 변환하는 transform 옵션 추가 3. 테스트의 설정을 beforeEach에서 beforeAll로 수정 beforeEach는 매 테스트 마다 데이터를 생성하고 수정하고 삭제하는 등 같은 작업을 한번 더 거치는 상황이었지만 beforeAll로써 생성 테스트에서 작업했던 데이터를 수정 삭제에서도 확인이 가능한 상태로 만듦

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,16 +2,36 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest'; // e2e 테스트
 import { AppModule } from './../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // beforeEach(async () => {
+  //   const moduleFixture: TestingModule = await Test.createTestingModule({
+  //     imports: [AppModule],
+  //   }).compile();
+  // 테스트의 설정이 beforeEach일 경우
+  // 매 테스트 마다 필요한 어플리케이션을 새로 생성하고, 테스트 후 삭제하게 된다.
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+    // 이를 beforeAll로 바꿈으로써 테스트 끼리 진행 상황을 공유한다.
+    // 예를 들어 POST 테스트에서 생성된 내용을 GET이나 PATCH에서도 별도 생성없이 테스트가 가능하다.
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true, // 데이터 타입의 자동 변환
+        // getOne이나 update 등 URL에 영화 ID를 입력할때
+        // string이던 파라미터를 number로 받아오던 기능을
+        // 테스트를 위해서도 사용
+      }),
+    );
     await app.init();
   });
 
@@ -24,7 +44,7 @@ describe('AppController (e2e)', () => {
 
   describe('/movies', () => {
     it('GET', () => {
-      return request(app.getHttpServer()) // http://localhost:3000/과 동일하다
+      return request(app.getHttpServer()) // http://localhost:3000/movies와 동일하다
         .get('/movies')
         .expect(200)
         .expect([]);
@@ -38,5 +58,16 @@ describe('AppController (e2e)', () => {
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it.todo('DELETE');
+    it.todo('PATCH');
   });
 });
